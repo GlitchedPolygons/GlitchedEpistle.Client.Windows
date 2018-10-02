936d2012531b0b45ff668d30277314becbc9938d
Glitched Polygons <glitchedpolygons@protonmail.com>
2018-10-02 22:29:22 +0200

    Nice background

--- GlitchedEpistle.Client.Windows/MainWindow.xaml.cs
+++ GlitchedEpistle.Client.Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GlitchedEpistle.Client.Windows
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
