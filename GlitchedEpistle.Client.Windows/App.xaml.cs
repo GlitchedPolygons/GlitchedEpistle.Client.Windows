81dd1822bab050109c27cd52bf9f4a1d08b75e09
GlitchedPolygons <glitchedpolygons@gmail.com>
2018-10-02 22:29:22 +0200

    Nice background

--- GlitchedEpistle.Client.Windows/App.xaml.cs
+++ GlitchedEpistle.Client.Windows/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace GlitchedEpistle.Client.Windows
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows
 {
     /// <summary>
     /// Interaction logic for App.xaml
