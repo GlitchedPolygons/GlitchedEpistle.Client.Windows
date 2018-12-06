aafe688b019716ee56562154bdb4fc0cdc4b067a
GlitchedPolygons <glitchedpolygons@gmail.com>
2018-12-06 14:32:04 +0100

    Started working on settings window.

---
+++ GlitchedEpistle.Client.Windows/Settings.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public string SettingsPath { get; }
+
+        public Settings()
+        {
+            InitializeComponent();
+            SettingsPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "GlitchedPolygons");
+        }
+
+        void Save()
+        {
+
+        }
+
+        void Load()
+        {
+
+        }
+
+        void UsernameTextBox_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            if (textBox == null || textBox.IsKeyboardFocusWithin)
+            {
+                return;
+            }
+            
+            e.Handled = true;
+            textBox.Focus();
+        }
+
+        private void UsernameTextBox_SelectText(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            textBox?.SelectAll();
+        }
+
+        void UsernameTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Save settings here
+           // throw new NotImplementedException();
+        }
+    }
+}
