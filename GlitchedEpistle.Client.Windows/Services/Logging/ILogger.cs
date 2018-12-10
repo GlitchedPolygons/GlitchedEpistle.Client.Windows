2a2f935e528a7ed428625ca4aaf6500580cee2d2
Raphael Beck <glitchedpolygons@gmail.com>
2018-12-10 23:54:19 +0100

    Added ILogger.cs and Logger.cs implementation.

---
+++ GlitchedEpistle.Client.Windows/Services/Logging/ILogger.cs
@@ -0,0 +1,12 @@
+﻿namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Services.Logging
+{
+    /// <summary>
+    /// Service interface for logging messages to their corresponding category's log file.
+    /// </summary>
+    public interface ILogger
+    {
+        void LogMessage(string msg);
+        void LogWarning(string msg);
+        void LogError(string msg);
+    }
+}
---
+++ GlitchedEpistle.Client.Windows/Services/Logging/Logger.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Services.Logging
+{
+    public class Logger : ILogger
+    {
+        /// <summary>
+        /// Gets the directory path where the log files are stored on disk.
+        /// </summary>
+        /// <value>The directory path where the log files are stored on disk..</value>
+        public string DirectoryPath { get; }
+
+        /// <summary>
+        /// Gets the message log file path.
+        /// </summary>
+        /// <value>The message log file path.</value>
+        public string MessageLogFilePath { get; }
+
+        /// <summary>
+        /// Gets the warning log file path.
+        /// </summary>
+        /// <value>The warning log file path.</value>
+        public string WarningLogFilePath { get; }
+
+        /// <summary>
+        /// Gets the error log file path.
+        /// </summary>
+        /// <value>The error log file path.</value>
+        public string ErrorLogFilePath { get; }
+
+        public Logger()
+        {
+            DirectoryPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "GlitchedPolygons",
+                "GlitchedEpistle",
+                "Logs"
+            );
+
+            MessageLogFilePath = Path.Combine(
+                DirectoryPath,
+                "Messages.log"
+            );
+
+            WarningLogFilePath = Path.Combine(
+                DirectoryPath,
+                "Warnings.log"
+            );
+
+            ErrorLogFilePath = Path.Combine(
+                DirectoryPath,
+                "Errors.log"
+            );
+        }
+
+        private void CheckDirectory()
+        {
+            if (!Directory.Exists(DirectoryPath))
+            {
+                Directory.CreateDirectory(DirectoryPath);
+            }
+        }
+
+        private string Timestamp(string msg) => $"[{DateTime.Now:s}] {msg}\n\n";
+
+        public void LogMessage(string msg)
+        {
+            CheckDirectory();
+            File.AppendAllText(MessageLogFilePath, Timestamp(msg));
+        }
+
+        public void LogWarning(string msg)
+        {
+            CheckDirectory();
+            File.AppendAllText(WarningLogFilePath, Timestamp(msg));
+        }
+
+        public void LogError(string msg)
+        {
+            CheckDirectory();
+            File.AppendAllText(ErrorLogFilePath, Timestamp(msg));
+        }
+    }
+}
