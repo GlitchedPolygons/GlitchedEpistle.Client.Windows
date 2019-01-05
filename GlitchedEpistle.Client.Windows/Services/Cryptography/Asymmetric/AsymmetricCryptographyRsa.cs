97a8bd582b115ccba98b783352353c832334cbc9
GlitchedPolygons <glitchedpolygons@gmail.com>
2019-01-05 03:41:28 +0100

    Added cryptography services

---
+++ GlitchedEpistle.Client.Windows/Services/Cryptography/Asymmetric/AsymmetricCryptographyRsa.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Services.Cryptography.Asymmetric
+{
+    /// <summary>
+    /// <see cref="IAsymmetricCryptographyRsa"/> implementation for asymmetric RSA encryption/decryption.
+    /// </summary>
+    /// <seealso cref="IAsymmetricCryptographyRsa" />
+    public class AsymmetricCryptographyRsa : IAsymmetricCryptographyRsa
+    {
+        /// <inheritdoc/>
+        public string Encrypt(string text, RSAParameters publicKey)
+        {
+            if (string.IsNullOrEmpty(text)) return null;
+
+            byte[] encryptedData;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(publicKey);
+                encryptedData = rsa.Encrypt(Encoding.UTF8.GetBytes(text), false);
+            }
+            return Convert.ToBase64String(encryptedData);
+        }
+
+        /// <inheritdoc/>
+        public string Decrypt(string encryptedText, RSAParameters privateKey)
+        {
+            if (string.IsNullOrEmpty(encryptedText)) return null;
+
+            byte[] data;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(privateKey);
+                if (rsa.PublicOnly)
+                {
+                    throw new CryptographicException($"{nameof(AsymmetricCryptographyRsa)}::{nameof(Decrypt)}: You've provided a public key instead of a private key... for decryption you need the private key!");
+                }
+                data = rsa.Decrypt(Convert.FromBase64String(encryptedText), false);
+            }
+            return Encoding.UTF8.GetString(data);
+        }
+
+        /// <inheritdoc/>
+        public byte[] Encrypt(byte[] data, RSAParameters publicKey)
+        {
+            byte[] encryptedData;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(publicKey);
+                encryptedData = rsa.Encrypt(data, false);
+            }
+            return encryptedData;
+        }
+
+        /// <inheritdoc/>
+        public byte[] Decrypt(byte[] encryptedData, RSAParameters privateKey)
+        {
+            byte[] data;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(privateKey);
+                if (rsa.PublicOnly)
+                {
+                    throw new CryptographicException($"{nameof(AsymmetricCryptographyRsa)}::{nameof(Decrypt)}: You've provided a public key instead of a private key... for decryption you need the private key!");
+                }
+                data = rsa.Decrypt(encryptedData, false);
+            }
+            return data;
+        }
+    }
+}
---
+++ GlitchedEpistle.Client.Windows/Services/Cryptography/Symmetric/ISymmetricCryptography.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Services.Cryptography.Asymmetric
+{
+    public interface ISymmetricCryptography
+    {
+
+    }
+}
