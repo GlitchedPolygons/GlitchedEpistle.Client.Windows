e68a14a34d7b940126e01dbc2c80dbd20c00cc3a
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-01-24 01:45:58 +0100

    Added account registration view.

---
+++ GlitchedEpistle.Client.Windows/Views/UserControls/RegisterView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for RegisterView.xaml
+    /// </summary>
+    public partial class RegisterView : UserControl
+    {
+        public RegisterView()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PasswordTextBox2_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UsernameTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
