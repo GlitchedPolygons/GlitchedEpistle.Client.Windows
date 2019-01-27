1fcc5313f5ecbb45cea5419c91f852a522a247ed
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-01-27 01:49:00 +0100

    Import file dialog

--- GlitchedEpistle.Client.Windows/ViewModels/UserControls/RegisterViewModel.cs
+++ GlitchedEpistle.Client.Windows/ViewModels/UserControls/RegisterViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ using GlitchedPolygons.GlitchedEpistle.Client.Extensions;
 using GlitchedPolygons.GlitchedEpistle.Client.Services.Users;
 using GlitchedPolygons.GlitchedEpistle.Client.Windows.Commands;
 using GlitchedPolygons.GlitchedEpistle.Client.Windows.PubSubEvents;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Services.Logging;
 using GlitchedPolygons.GlitchedEpistle.Client.Windows.Services.Settings;
 using GlitchedPolygons.Services.CompressionUtility;
+using Microsoft.Win32;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.OpenSsl;
@@ -23,6 +26,7 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControl
     public class RegisterViewModel : ViewModel
     {
         #region Constants
+        private readonly ILogger logger;
         private readonly ISettings settings;
         private readonly IUserService userService;
         private readonly ICompressionUtility gzip;
@@ -54,9 +58,10 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControl
         private bool pendingAttempt = false;
         private Timer ErrorMessageTimer { get; } = new Timer(ERROR_MESSAGE_INTERVAL) { AutoReset = true };
 
-        public RegisterViewModel(IUserService userService, ISettings settings, IEventAggregator eventAggregator, ICompressionUtility gzip)
+        public RegisterViewModel(IUserService userService, ISettings settings, IEventAggregator eventAggregator, ICompressionUtility gzip, ILogger logger)
         {
             this.gzip = gzip;
+            this.logger = logger;
             this.settings = settings;
             this.userService = userService;
             this.eventAggregator = eventAggregator;
@@ -77,7 +82,26 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControl
 
         private void OnClickedImport(object commandParam)
         {
-            // TODO: open file dialog here and import user data from backup file
+            var dialog = new OpenFileDialog
+            {
+                Title = "Epistle backup file path",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                DefaultExt = ".dat",
+                AddExtension = true,
+                Filter = "Epistle Backup File|*.dat;*.dat.gz"
+            };
+            dialog.ShowDialog();
+
+            if (string.IsNullOrEmpty(dialog.FileName))
+            {
+                return;
+            }
+
+            // TODO: see comment on new line
+            // Import user data from backup file here and then reboot the program.
+
+            Application.Current.Shutdown();
+            Process.Start(Application.ResourceAssembly.Location);
         }
 
         private async void OnClickedRegister(object commandParam)
@@ -89,7 +113,7 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControl
             {
                 return;
             }
-
+            
             pendingAttempt = true;
 
             Directory.CreateDirectory(KEYS_DIR);
@@ -130,6 +154,7 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControl
 
             var userCreationResponse = await userService.CreateUser(Password, pubKeyXml, userCreationSecret);
             eventAggregator.GetEvent<UserCreationSucceededEvent>().Publish(userCreationResponse);
+            logger.LogMessage($"Created user {userCreationResponse.Id}.");
             // Handle this event back in the main view model, since it's there where the backup codes + 2FA secret (QR) will be shown.
 
             pendingAttempt = false;
