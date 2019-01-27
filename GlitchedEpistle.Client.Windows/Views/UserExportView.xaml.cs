6467c5a5d799a54bb04dedfe937698493b653e08
Raphael Beck <glitchedpolygons@gmail.com>
2019-01-27 21:54:30 +0100

    Extracted export user file path dialog from code-behind into viewmodel.

--- GlitchedEpistle.Client.Windows/Views/UserExportView.xaml.cs
+++ GlitchedEpistle.Client.Windows/Views/UserExportView.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows;
 using GlitchedPolygons.GlitchedEpistle.Client.Windows.Extensions;
-using Microsoft.Win32;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views
 {
@@ -20,31 +19,6 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views
         {
             this.MakeCloseable();
             Loaded -= OnLoaded;
-        }
-
-        private void BrowseButton_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new SaveFileDialog
-            {
-                Title = "Epistle backup file path",
-                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                FileName = $"{DateTime.Now:yyyy-MM-dd-HH-mm}-glitched-epistle-backup.dat",
-                DefaultExt = ".dat",
-                AddExtension = true,
-                OverwritePrompt = true,
-                Filter = "Epistle Backup File|*.dat"
-            };
-            dialog.FileOk += FileDialog_FileOk;
-            dialog.ShowDialog(this);
-        }
-
-        private void FileDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            if (sender is SaveFileDialog dialog)
-            {
-                dialog.FileOk -= FileDialog_FileOk;
-                OutputFilePathTextBox.Text = dialog.FileName;
-            }
         }
     }
 }
