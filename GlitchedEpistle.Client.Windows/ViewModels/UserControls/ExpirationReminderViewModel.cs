f1e55f610fa119a7b1056c134d81eaf57406f059
Raphael Beck <glitchedpolygons@gmail.com>
2019-02-05 01:23:30 +0100

    Prepared reminder vm

--- GlitchedEpistle.Client.Windows/ViewModels/UserControls/ExpirationReminderViewModel.cs
+++ GlitchedEpistle.Client.Windows/ViewModels/UserControls/ExpirationReminderViewModel.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
+
+using GlitchedPolygons.GlitchedEpistle.Client.Models;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Views;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Commands;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Services.Factories;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControls
 {
     public class ExpirationReminderViewModel : ViewModel
     {
+        #region Constants
+        // Injections:
+        private readonly User user;
+        private readonly IWindowFactory windowFactory;
+        private readonly IViewModelFactory viewModelFactory;
+        #endregion
+
+        #region Commands
+        public ICommand ExtendButtonCommand { get; }
+        #endregion
+
+        #region UI Bindings
+        private string reminderText;
+        public string ReminderText { get => reminderText; set => Set(ref reminderText, value); }
+        #endregion
+
+        public ExpirationReminderViewModel(User user, IWindowFactory windowFactory, IViewModelFactory viewModelFactory)
+        {
+            this.user = user;
+            this.windowFactory = windowFactory;
+            this.viewModelFactory = viewModelFactory;
+
+            ExtendButtonCommand = new DelegateCommand(OnClickedExtend);
+        }
+
+        private void OnClickedExtend(object commandParam)
+        {
+            var settingsView = windowFactory.Create<SettingsView>(true);
+            if (settingsView.DataContext is null)
+            {
+                settingsView.DataContext = viewModelFactory.Create<SettingsViewModel>();
+            }
+            settingsView.Show();
+            settingsView.Activate();
+        }
     }
 }
