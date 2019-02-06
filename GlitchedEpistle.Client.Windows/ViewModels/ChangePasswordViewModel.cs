648b5241933fbd00cb743890c0279a9f79680aa4
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-02-06 14:57:40 +0100

    Prepared change pw dialog window + viewmodel (needs definite implementation)

---
+++ GlitchedEpistle.Client.Windows/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+using GlitchedPolygons.GlitchedEpistle.Client.Services.Users;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Commands;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels
+{
+    public class ChangePasswordViewModel : ViewModel, ICloseable
+    {
+        #region Constants
+        private readonly IUserService userService;
+        #endregion
+
+        #region Events
+        public event EventHandler<EventArgs> RequestedClose;
+        #endregion
+
+        #region Commands
+        public ICommand CancelCommand { get; }
+        public ICommand SubmitCommand { get; }
+        #endregion
+
+        #region UI Bindings
+        private string oldPw;
+        public string OldPassword { get => oldPw; set => Set(ref oldPw, value); }
+
+        private string newPw;
+        public string NewPassword { get => newPw; set => Set(ref newPw, value); }
+
+        private string newPw2;
+        public string NewPassword2 { get => newPw2; set => Set(ref newPw2, value); }
+        #endregion
+
+        public ChangePasswordViewModel(IUserService userService)
+        {
+            this.userService = userService;
+            SubmitCommand = new DelegateCommand(SubmitChangePassword);
+            CancelCommand = new DelegateCommand(o => RequestedClose?.Invoke(null, null));
+        }
+
+        private void SubmitChangePassword(object commandParam)
+        {
+            if (string.IsNullOrEmpty(OldPassword) 
+                || string.IsNullOrEmpty(NewPassword) 
+                || NewPassword != NewPassword2)
+            {
+                return;
+            }
+
+            // TODO: change pw here using IUserService asap!
+        }
+    }
+}
---
+++ GlitchedEpistle.Client.Windows/Views/ChangePasswordView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Extensions;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views
+{
+    /// <summary>
+    /// Interaction logic for ChangePasswordView.xaml
+    /// </summary>
+    public partial class ChangePasswordView : Window
+    {
+        public ChangePasswordView()
+        {
+            InitializeComponent();
+            this.MakeCloseable();
+        }
+    }
+}
