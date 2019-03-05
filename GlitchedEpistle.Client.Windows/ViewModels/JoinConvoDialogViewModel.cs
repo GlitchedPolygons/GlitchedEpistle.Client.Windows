8bc2d36393dad886840c2cd3afbb6d07cad34503
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-03-05 23:05:37 +0100

    Enabled enter key for submitting convo creation inside CreateConvoView.xaml + added JoinConvoDialogView.xaml - ready for implementation

---
+++ GlitchedEpistle.Client.Windows/ViewModels/JoinConvoDialogViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+using GlitchedPolygons.GlitchedEpistle.Client.Extensions;
+using GlitchedPolygons.GlitchedEpistle.Client.Services.Convos;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Commands;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels
+{
+    public class JoinConvoDialogViewModel : ViewModel, ICloseable
+    {
+        #region Constants
+        private readonly Timer messageTimer = new Timer(7000) { AutoReset = true };
+
+        // Injections:
+        private readonly IConvoService convoService;
+        private readonly IConvoProvider convoProvider;
+        #endregion
+
+        #region Events
+        public event EventHandler<EventArgs> RequestedClose;
+        #endregion
+
+        #region Commands
+        public ICommand JoinCommand { get; }
+        public ICommand CancelCommand { get; }
+        #endregion
+
+        #region UI Bindings
+        private string errorMessage = string.Empty;
+        public string ErrorMessage { get => errorMessage; set => Set(ref errorMessage, value); }
+
+        private string convoId = string.Empty;
+        public string ConvoId { get => convoId; set => Set(ref convoId, value); }
+        #endregion
+
+        public JoinConvoDialogViewModel(IConvoService convoService, IConvoProvider convoProvider)
+        {
+            this.convoService = convoService;
+            this.convoProvider = convoProvider;
+
+            JoinCommand = new DelegateCommand(OnClickedJoinConvo);
+            CancelCommand = new DelegateCommand(_ => RequestedClose?.Invoke(null, EventArgs.Empty));
+
+            messageTimer.Elapsed += (_, __) => ErrorMessage = null;
+            messageTimer.Start();
+        }
+
+        private void ResetMessages()
+        {
+            messageTimer.Stop();
+            messageTimer.Start();
+            ErrorMessage = null;
+        }
+
+        private void OnClickedJoinConvo(object commandParam)
+        {
+            if (commandParam is PasswordBox passwordBox)
+            {
+                string pw = passwordBox.Password.SHA512();
+
+            }
+        }
+    }
+}
---
+++ GlitchedEpistle.Client.Windows/Views/JoinConvoDialogView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Extensions;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views
+{
+    /// <summary>
+    /// Interaction logic for <c>JoinConvoDialogView.xaml</c>
+    /// </summary>
+    public partial class JoinConvoDialogView : Window
+    {
+        public JoinConvoDialogView()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.Loaded -= OnLoaded;
+            this.MakeCloseable();
+        }
+    }
+}
