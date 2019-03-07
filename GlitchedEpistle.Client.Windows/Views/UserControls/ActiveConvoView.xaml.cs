4f0d38cefbfa7cd060535686108bd70be68e2269
GlitchedPolygons <glitchedpolygons@gmail.com>
2019-03-07 01:57:45 +0100

    wip convo ui control

--- GlitchedEpistle.Client.Windows/Views/UserControls/ActiveConvoView.xaml.cs
+++ GlitchedEpistle.Client.Windows/Views/UserControls/ActiveConvoView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views.UserControls
 {
@@ -23,6 +11,12 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views.UserControls
         public ActiveConvoView()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            TextBox.Focus();
         }
     }
 }
