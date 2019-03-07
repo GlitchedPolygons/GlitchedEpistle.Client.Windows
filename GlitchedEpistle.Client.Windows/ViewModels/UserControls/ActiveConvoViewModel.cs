db82e0f9010563dc4356aa153a5bd3c020ee8eb5
GlitchedPolygons <glitchedpolygons@gmail.com>
2019-03-07 02:30:06 +0100

    Added copy to clipboard button + corresponding confirmation tick label functionality to convo user control

--- GlitchedEpistle.Client.Windows/ViewModels/UserControls/ActiveConvoViewModel.cs
+++ GlitchedEpistle.Client.Windows/ViewModels/UserControls/ActiveConvoViewModel.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using GlitchedPolygons.GlitchedEpistle.Client.Models;
 using GlitchedPolygons.GlitchedEpistle.Client.Services.Convos;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Commands;
+using GlitchedPolygons.Services.MethodQ;
 using Prism.Events;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControls
@@ -19,14 +23,40 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControl
         private readonly IEventAggregator eventAggregator;
         #endregion
 
+        #region Commands
+        public ICommand CopyConvoIdToClipboardCommand { get; set; }
+        #endregion
+
+        #region UI Bindings
+        private Visibility clipboardTickVisibility = Visibility.Hidden;
+        public Visibility ClipboardTickVisibility { get => clipboardTickVisibility; set => Set(ref clipboardTickVisibility, value); }
+        #endregion
+
         public Convo ActiveConvo { get; set; }
 
-        public ActiveConvoViewModel(User user, IConvoService convoService, IConvoProvider convoProvider, IEventAggregator eventAggregator)
+        private ulong? scheduledHideGreenTickIcon = null;
+
+        public ActiveConvoViewModel(User user, IConvoService convoService, IConvoProvider convoProvider, IEventAggregator eventAggregator, IMethodQ methodQ)
         {
             this.user = user;
             this.convoService = convoService;
             this.convoProvider = convoProvider;
             this.eventAggregator = eventAggregator;
+
+            CopyConvoIdToClipboardCommand = new DelegateCommand(_ =>
+            {
+                Clipboard.SetText(ActiveConvo.Id);
+                ClipboardTickVisibility = Visibility.Visible;
+
+                if (scheduledHideGreenTickIcon.HasValue)
+                    methodQ.Cancel(scheduledHideGreenTickIcon.Value);
+
+                scheduledHideGreenTickIcon = methodQ.Schedule(() =>
+                {
+                    ClipboardTickVisibility = Visibility.Hidden;
+                    scheduledHideGreenTickIcon = null;
+                }, DateTime.UtcNow.AddSeconds(3));
+            });
         }
     }
 }
