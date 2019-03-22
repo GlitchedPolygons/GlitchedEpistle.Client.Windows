d640319135ee22c391c58e6b55df16cf88b1495b
Raphael Beck <glitchedpolygons@gmail.com>
2019-03-23 00:29:33 +0100

    WIP theme switching method extraction (into App.xaml.cs)

--- GlitchedEpistle.Client.Windows/App.xaml.cs
+++ GlitchedEpistle.Client.Windows/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 using System.Threading;
 using System.Windows;
 
+using GlitchedPolygons.GlitchedEpistle.Client.Extensions;
 using GlitchedPolygons.GlitchedEpistle.Client.Models;
 using GlitchedPolygons.GlitchedEpistle.Client.Services.Convos;
 using GlitchedPolygons.GlitchedEpistle.Client.Services.Coupons;
@@ -24,6 +26,8 @@ using GlitchedPolygons.Services.CompressionUtility;
 using GlitchedPolygons.Services.JwtService;
 using GlitchedPolygons.Services.MethodQ;
 
+using Org.BouncyCastle.Crypto.Digests;
+
 using Prism.Events;
 
 using Unity;
@@ -40,6 +44,12 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows
         /// The client version number.
         /// </summary>
         public const string VERSION = "1.0.0";
+
+        /// <summary>
+        /// Gets the currently active GUI theme (appearance of the app).
+        /// </summary>
+        /// <value>The current theme.</value>
+        public string CurrentTheme { get; private set; } = Themes.DARK_THEME;
 
         /// <summary>
         /// The IoC container.
@@ -84,6 +94,9 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows
             mainView.DataContext = container.Resolve<MainViewModel>();
             Current.MainWindow = mainView;
             Current.MainWindow?.Show();
+
+            var settings = container.Resolve<ISettings>();
+            ChangeTheme(settings["Theme", Themes.DARK_THEME]);
         }
 
         /// <summary>
@@ -94,6 +107,47 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows
         public T Resolve<T>() where T : class
         {
             return container?.Resolve<T>();
+        }
+
+        /// <summary>
+        /// Changes the application GUI's theme.
+        /// </summary>
+        /// <param name="theme">The theme to switch to.</param>
+        /// <returns>Whether the theme change occurred or not (e.g. in case of changing to a theme that's already active, or in case of a failure, this method returns <c>false</c>).</returns>
+        public bool ChangeTheme(string theme)
+        {
+            string path = null;
+            ILogger logger = container?.Resolve<ILogger>();
+
+            switch (theme)
+            {
+                case Themes.DARK_THEME:
+                    path = "/Resources/Themes/DarkTheme.xaml";
+                    break;
+                case Themes.LIGHT_THEME:
+                    path = "/Resources/Themes/LightTheme.xaml";
+                    break;
+                case Themes.OLED_THEME:
+                    path = "/Resources/Themes/OLEDTheme.xaml";
+                    break;
+            }
+
+            if (path.NullOrEmpty())
+            {
+                logger?.LogWarning($"Theme \"{theme}\" couldn't be found/does not exist.");
+                return false;
+            }
+
+            try
+            {
+                Resources.MergedDictionaries[0] = new ResourceDictionary { Source = new Uri(path, UriKind.Relative) };
+                return true;
+            }
+            catch (Exception e)
+            {
+                logger?.LogWarning($"Theme \"{path}\" couldn't be applied. Reverting to default theme... Thrown exception: {e.ToString()}");
+                return false;
+            }
         }
     }
 }
