95632c83b088bbd0087bf828e91de3e8c347d7bd
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-03-23 12:39:25 +0100

    WIP edit convo metadata view + viewmodel - TO DO: check this commit, might be flawed. People around me talking to me, wanting shit from me. fucking disturbing.

--- GlitchedEpistle.Client.Windows/ViewModels/EditConvoMetadataViewModel.cs
+++ GlitchedEpistle.Client.Windows/ViewModels/EditConvoMetadataViewModel.cs
@@ -1,12 +1,96 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Input;
+
+using GlitchedPolygons.GlitchedEpistle.Client.Models;
+using GlitchedPolygons.GlitchedEpistle.Client.Extensions;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Commands;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels
 {
-    public class EditConvoMetadataViewModel : ViewModel
+    public class EditConvoMetadataViewModel : ViewModel, ICloseable
     {
+        #region Constants
+        private readonly Timer messageTimer = new Timer(7000) { AutoReset = true };
+        #endregion
+
+        #region Events
+        public event EventHandler<EventArgs> RequestedClose;
+        #endregion
+
+        #region Commands
+        public ICommand CancelCommand { get; }
+        public ICommand SubmitCommand { get; }
+        #endregion
+
+        #region UI Bindings
+        private string errorMessage = string.Empty;
+        public string ErrorMessage
+        {
+            get => errorMessage;
+            set => Set(ref errorMessage, value);
+        }
+
+        private string successMessage = string.Empty;
+        public string SuccessMessage
+        {
+            get => successMessage;
+            set => Set(ref successMessage, value);
+        }
+        #endregion
+
+        public Convo Convo { get; set; }
+
+        public EditConvoMetadataViewModel()
+        {
+            SubmitCommand = new DelegateCommand(OnSubmit);
+            CancelCommand = new DelegateCommand(OnCancel);
+
+            messageTimer.Elapsed += (_, __) => ResetMessages();
+            messageTimer.Start();
+        }
+
+        private void ResetMessages()
+        {
+            messageTimer.Stop();
+            messageTimer.Start();
+            ErrorMessage = SuccessMessage = null;
+        }
+
+        private void PrintMessage(string message, bool error)
+        {
+            Application.Current?.Dispatcher?.Invoke(() =>
+            {
+                ResetMessages();
+
+                if (error)
+                {
+                    ErrorMessage = message;
+                }
+                else
+                {
+                    SuccessMessage = message;
+                }
+            });
+        }
+
+        private void OnCancel(object commandParam)
+        {
+            RequestedClose?.Invoke(null, EventArgs.Empty);
+        }
+
+        private void OnSubmit(object commandParam)
+        {
+            var totp = commandParam as string;
+
+            if (totp.NullOrEmpty())
+            {
+                PrintMessage("No 2FA token provided - please take security seriously and authenticate your request!", true);
+                return;
+            }
+
+            // TODO: submit here
+        }
     }
 }
--- GlitchedEpistle.Client.Windows/Views/EditConvoMetadataView.xaml.cs
+++ GlitchedEpistle.Client.Windows/Views/EditConvoMetadataView.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Extensions;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views
 {
@@ -22,6 +12,12 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views
         public EditConvoMetadataView()
         {
             InitializeComponent();
+            Loaded += EditConvoMetadataView_Loaded;
+        }
+
+        private void EditConvoMetadataView_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.MakeCloseable();
         }
     }
 }
