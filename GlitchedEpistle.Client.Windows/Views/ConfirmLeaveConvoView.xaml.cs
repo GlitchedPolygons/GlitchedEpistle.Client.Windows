0e5bcdac8f1b3d4b7fc8efce2c20e8eb98c82147
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-03-24 23:19:50 +0100

    Implemented EditConvoMetadataViewModel::OnLeave

---
+++ GlitchedEpistle.Client.Windows/Views/ConfirmLeaveConvoView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.Views
+{
+    /// <summary>
+    /// Interaction logic for ConfirmLeaveConvoView.xaml
+    /// </summary>
+    public partial class ConfirmLeaveConvoView : Window
+    {
+        public ConfirmLeaveConvoView()
+        {
+            InitializeComponent();
+        }
+
+        private void LeaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
