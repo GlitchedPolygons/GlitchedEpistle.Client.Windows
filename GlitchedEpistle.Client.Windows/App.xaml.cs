bad4d7bcfd523c03119e143bb3c248932fff929f
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-07-02 01:41:01 +0200

    Release 1.1.0.1

--- GlitchedEpistle.Client.Windows/App.xaml.cs
+++ GlitchedEpistle.Client.Windows/App.xaml.cs
@@ -43,7 +43,7 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows
         /// <summary>
         /// The client version number.
         /// </summary>
-        public const string VERSION = "1.1.0";
+        public const string VERSION = "1.1.0.1";
 
         /// <summary>
         /// Gets the currently active GUI theme (appearance of the app).
