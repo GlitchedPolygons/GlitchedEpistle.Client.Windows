c393f051cb5f921a50b73e2c9c5be9499a765606
Raphael Beck <glitchedpolygons@gmail.com>
2019-08-06 03:03:49 +0200

    docs/

--- GlitchedEpistle.Client.Windows/PubSubEvents/CouponRedeemedEvent.cs
+++ GlitchedEpistle.Client.Windows/PubSubEvents/CouponRedeemedEvent.cs
@@ -3,7 +3,8 @@
 namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.PubSubEvents
 {
     /// <summary>
-    /// This parameterless <see cref="PubSubEvent"/> is raised whenever the user has successfully redeemed a coupon code to extend his account.
+    /// This parameterless <see cref="PubSubEvent"/> is raised
+    /// whenever the user has successfully redeemed a coupon code to extend his account.
     /// </summary>
     /// <seealso cref="Prism.Events.PubSubEvent" />
     public class CouponRedeemedEvent : PubSubEvent
