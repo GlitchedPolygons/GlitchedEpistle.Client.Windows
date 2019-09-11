48f28f50616d54678cfd55c166a8d0cb573c9791
Raphael Beck <glitchedpolygons@gmail.com>
2019-09-11 03:06:46 +0200

    wip

--- src/ViewModels/UserControls/ServerUrlViewModel.cs
+++ src/ViewModels/UserControls/ServerUrlViewModel.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Input;
 
 using GlitchedPolygons.GlitchedEpistle.Client.Services.Web.ServerHealth;
+using GlitchedPolygons.GlitchedEpistle.Client.Utilities;
+using GlitchedPolygons.GlitchedEpistle.Client.Windows.Commands;
 
 namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControls
 {
@@ -12,9 +10,27 @@ namespace GlitchedPolygons.GlitchedEpistle.Client.Windows.ViewModels.UserControl
     {
         private readonly IServerConnectionTest test;
 
+        #region Commands
+        public ICommand ConnectCommand { get; }
+        #endregion
+
+        #region UI Bindings
+        private string serverUrl = "https://epistle.glitchedpolygons.com";
+        public string ServerUrl { get => serverUrl; set => Set(ref serverUrl, value); }
+        #endregion
+
         public ServerUrlViewModel(IServerConnectionTest test)
         {
             this.test = test;
+            ConnectCommand = new DelegateCommand(OnClickedConnect);
+        }
+
+        private async void OnClickedConnect(object commandParam)
+        {
+            UrlUtility.SetEpistleServerUrl(ServerUrl);
+            bool success = await test.TestConnection();
+
+            // TODO: handle both success and failure here
         }
     }
 }
